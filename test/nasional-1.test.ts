8bdf228e8616550d485846f8b0b26164cf9ac668
Achmad Ilmi Al Akbar <113961961+notilmi@users.noreply.github.com>
2024-04-26 08:54:03 +0700

    Initial commit

---
+++ test/nasional-1.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Nasional1 from '../lib/nasional-1-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/nasional-1-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Nasional1.Nasional1Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
